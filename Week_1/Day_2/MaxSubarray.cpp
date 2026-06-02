3ffde8a5cb39a909228bc1d202ffb421dd5ac6b4
Saumya Yadav <saumyayadav2005@gmail.com>
2026-06-02 20:29:32 +0530

    Added Maximum Subarray Kadanes solution

---
+++ Week_1/Day_2/MaxSubarray.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN; 
+        int currentSum = 0;   
+        
+        for (int i = 0; i < nums.size(); i++) {
+            currentSum += nums[i]; // Naya element
+            
+            
+            maxSum = max(maxSum, currentSum);
+            
+           
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+};

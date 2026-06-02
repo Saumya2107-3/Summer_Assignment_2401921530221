76c0fd670b0734f0d3cc68625f20f6edba98f8fc
Saumya2107-3 <saumyayadav2005@gmail.com>
2026-06-02 20:38:37 +0530

    Update MaxAverageSubarray.cpp

--- Week_1/Day_2/MaxAverageSubarray.cpp
+++ Week_1/Day_2/MaxAverageSubarray.cpp
@@ -8,21 +8,21 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         double sum = 0;
         
-        // 1. Pehle k elements ka sum nikal lo (Pehli Window)
+       
         for (int i = 0; i < k; i++) {
             sum += nums[i];
         }
         
         double maxSum = sum;
         
-        // 2. Sliding Window: Ek-ek kadam aage badho
+        
         for (int i = k; i < nums.size(); i++) {
-            // Naya element jodenge, aur purana peeche wala element ghata denge
+            
             sum += nums[i] - nums[i - k]; 
             maxSum = max(maxSum, sum);
         }
         
-        // 3. Sabse bade sum ko k se divide karke Average return karo
+        
         return maxSum / k;
     }
 };

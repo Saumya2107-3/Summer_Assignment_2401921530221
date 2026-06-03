2e404025b9f7d7d989ccf4036c2a4e89b496d759
Saumya Yadav <saumyayadav2005@gmail.com>
2026-06-03 18:43:20 +0530

    Completed Day 3 Two Pointer Assignment

---
+++ Week_1/Day_3/MoveZeroes.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int insertPos = 0; 
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+            if (nums[i] != 0) {
+                swap(nums[insertPos], nums[i]);
+                insertPos++;
+            }
+        }
+    }
+};

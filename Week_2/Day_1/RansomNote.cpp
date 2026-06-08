fe990e42eea8bb0845d400fe198d4d3d91a00110
Saumya Yadav <saumyayadav2005@gmail.com>
2026-06-08 19:25:43 +0530

    Completed Week 2 Day 1 Hashing assignment

---
+++ Week_2/Day_1/RansomNote.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> count(26, 0);
+        
+        
+        for (char c : magazine) {
+            count[c - 'a']++;
+        }
+        
+        
+        for (char c : ransomNote) {
+            
+            if (count[c - 'a'] == 0) {
+                return false;
+            }
+            count[c - 'a']--; 
+        }
+        
+        return true;
+    }
+};

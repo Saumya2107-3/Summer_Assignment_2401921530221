e48c5b7c9fec897841a40ff1a3f9c21818904bef
Saumya Yadav <saumyayadav2005@gmail.com>
2026-06-22 20:37:12 +0530

    Completed Week 4 Day 1 Binary Tree basics assignment

---
+++ Week_4/Day_1/InvertBinaryTree.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        
+        if (root == NULL) return NULL;
+        
+        
+        swap(root->left, root->right);
+        
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        return root; 
+    }
+};
